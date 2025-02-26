b64e363e54ecb16d312a708ab04e72cb9ba8fc2e
Fabiano Souza <fabianokurashi@gmail.com>
2025-02-26 20:54:53 -0300

    build: icon react data picker

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,6 +1,6 @@
 import type { Config } from "tailwindcss";
 
-export default {
+const config: Config = {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
@@ -9,10 +9,19 @@ export default {
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        primary: '#0080FF',
+        text: '#00264D',
+        caption: '#698096',
+        gray: '#BFCCD9',
+        lightGray: '#E3EBF3',
+        white: '#FFFFFF',
+        overlay: 'rgba(18, 57, 82, 0.95)',
+        yellow: '#F2BF09',
+        red: '#FF0000',
       },
     },
   },
   plugins: [],
-} satisfies Config;
+};
+
+export default config;
